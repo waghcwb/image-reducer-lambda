cef836c50087e0859dc7eaf900ddcb9f0033f18c
Wagner Souza <wagner.souza@accenture.com>
2020-01-27 15:06:23 -0300

    test

--- functions/img.js
+++ functions/img.js
@@ -1,4 +1,17 @@
+const consola = require('consola')
+
 exports.handler = (event, context, callback) => {
+    const allowedMethods = ['GET']
+
+    consola.log(event)
+
+    if (allowedMethods.includes(event.httpMethod)) {
+        return {
+            statusCode: 405,
+            body: 'go away!'
+        }
+    }
+
     callback(null, {
         statusCode: 200,
         body: 'No worries, all is working fine!'
