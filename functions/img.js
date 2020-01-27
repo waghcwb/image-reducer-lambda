406eccaca2de7e64c728d7098d3b9a9af5abdbdd
Wagner Souza <wagner.souza@accenture.com>
2020-01-27 15:30:01 -0300

    testing

--- functions/img.js
+++ functions/img.js
@@ -1,8 +1,11 @@
-exports.handler = (event, context, callback) => {
-    const allowedMethods = ['GET']
+const debug = require('debug')('imagereducer:app')
+const _request = require('request').defaults({ encoding: null })
+const { promisify } = require('util')
 
-    console.log('test log')
-    console.warn('test log')
+const request = promisify(_request)
+
+exports.handler = async (event, context, callback) => {
+    const allowedMethods = ['GET']
 
     if (!allowedMethods.includes(event.httpMethod)) {
         return {
@@ -13,9 +16,16 @@ exports.handler = (event, context, callback) => {
 
     const [ width, base64 ] = event.path.split('/').slice(4)
     const base64Buffer = new Buffer(base64, 'base64')
+    const img = base64Buffer.toString('ascii')
+
+    const { body, statusCode } = await request(img)
 
     callback(null, {
-        statusCode: 200,
-        body: 'No worries, all is working fine!\n' + `width: ${width}\n base64: ${base64}` + `\nbase64Buffer: ${base64Buffer}`
+        statusCode: statusCode,
+        headers: {
+            'content-type': 'image/png',
+            'Content-Length': body.length
+        },
+        body: Buffer.from(body)
     })
 }
